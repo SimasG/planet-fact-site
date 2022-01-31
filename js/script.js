5ffbba64fc7e8fb35d545430cef553df952129c3
simutis <54304022+SimasG@users.noreply.github.com>
2022-01-31 13:16:41 +0200

    progressed on the tablet header

--- js/script.js
+++ js/script.js
@@ -3,12 +3,16 @@
 const hamburger = document.querySelector('.header__toggle');
 const navUl = document.querySelector('.header__menu');
 const logo = document.querySelector('.header__logo');
-const underline = document.querySelector('.header__underline');
+// const underline = document.querySelector('.header__underline');
+// const underlineTwo = document.querySelector('.header__menu-hr-line');
+
 
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navUl.classList.toggle('active');
     logo.classList.toggle('active');
-    underline.classList.toggle('active');
+    // underline.classList.toggle('active');
+    // underlineTwo.classList.toggle('active');
+
 })
