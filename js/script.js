12e0c0a531fb6ddbb691e4ae705c8eb611e49840
simutis <54304022+SimasG@users.noreply.github.com>
2022-01-31 15:11:03 +0200

    finished desktop header

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 // hamburger menu toggle
-
 const hamburger = document.querySelector('.header__toggle');
 const navUl = document.querySelector('.header__menu');
 const logo = document.querySelector('.header__logo');
-// const underline = document.querySelector('.header__underline');
-// const underlineTwo = document.querySelector('.header__menu-hr-line');
 
 
 
@@ -12,7 +9,16 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navUl.classList.toggle('active');
     logo.classList.toggle('active');
-    // underline.classList.toggle('active');
-    // underlineTwo.classList.toggle('active');
-
 })
+
+
+// planet pseudo elements
+// const planetUnderline = document.querySelectorAll('.planet-name'), ':after';
+
+// planetUnderline.addEventListener('click', () => {
+//     console.log("whatsuppppp");
+// })
+
+
+
+// tabs
