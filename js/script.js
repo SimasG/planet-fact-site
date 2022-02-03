1efd654b1dec1d69cc7af9a43fd1a7ec65678769
simutis <54304022+SimasG@users.noreply.github.com>
2022-02-03 10:50:16 +0200

    progressed on manipulating json data

--- js/script.js
+++ js/script.js
@@ -1,3 +1,58 @@
+// JSON DATA
+
+// You can only use Fetch API when trying to fetch external files outside of your machine, not local ones.
+// That's why you have to start a live server instead of developing using files from files:///
+
+// ./data.json -> an argument of the file location you want to access
+// const json = fetch('./data.json')
+    // returning a promise that resolves with a Response object. 
+//   .then(response => response.json())
+    // using json() method to extract the JSON body content from the Response
+    // second promise resolves with the result of parsing the response body text as JSON.
+//   .then(json => console.log(json));
+
+
+
+// planets
+
+
+const venus = document.getElementById('planet-venus');
+
+const H1 = document.querySelector('.main__text-heading');
+
+
+venus.addEventListener('click', () => {
+    async function populate() {
+        const response = await fetch('./data.json');
+        const planetsJson = await response.json()
+    
+        console.log(planetsJson)
+
+    }
+
+    populate();
+})
+
+
+function displayPlanets(items) {
+    let displayPlanet = items.map(function (item) {
+
+        return `<h1>${item.name}</h1>`
+    })
+
+    displayPlanet = displayPlanet.join("");
+    H1.innerHTML = displayPlanet;
+
+}
+
+
+
+
+
+
+
+
+
 // hamburger menu toggle
 const hamburger = document.querySelector('.header__toggle');
 const navUl = document.querySelector('.header__menu');
@@ -33,9 +88,7 @@ const mainContainer = document.querySelector('.main__container');
 const contents = document.querySelectorAll('.main__hero');
 
 mainContainer.addEventListener('click', (e) => {
-    console.log("eyooo");
     const id = e.target.dataset.id;
-    console.log(id);
 
     if (id) {
         tabs.forEach(btn => {
@@ -51,24 +104,3 @@ mainContainer.addEventListener('click', (e) => {
         element.classList.add('active');
     }
 })
-
-
-// JSON DATA
-
-
-// require -> nodejs built-in function
-
-// const jsonData = require('../data.json');
-// console.log(jsonData);
-
-// You can only use fetch when trying to fetch external files outside of your machine, not local ones.
-// That's why you have to start a live server instead of developing using files from files:///
-
-// ./data.json -> an argument of the file location you want to access
-const json = fetch('./data.json')
-    // returning a promise that resolves with a Response object. 
-  .then(response => response.json())
-    // using json() method to extract the JSON body content from the Response
-    // second promise resolves with the result of parsing the response body text as JSON.
-  .then(json => console.log(json));
-
